d82e199f2f83217662e1c6e97dcb3b28d6357f66
Andrew G. Saushkin <asaushkin@gmail.com>
2016-12-27 13:28:46 +0300

    servlet logger

--- src/main/java/me/asaushkin/BaseServlet.java
+++ src/main/java/me/asaushkin/BaseServlet.java
@@ -6,16 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Servlet implementation class BaseServlet
  */
 public class BaseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Logger logger = LoggerFactory.getLogger(BaseServlet.class);
        
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    logger.debug("Base servlet");
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
